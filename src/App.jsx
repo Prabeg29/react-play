54f480f174ddfce0fa148c844cf3dc71f5b86231
Prabeg Shakya <shakyaprabeg@gmail.com>
2023-06-07 15:02:26 +0545

    Add tasks component along with global state
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
+
+
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 const App = () => {
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: 'Fix bug',
+        day: 'Feb 5th at 2.30 pm',
+        reminder: true
+      },
+      {
+        id: 2,
+        text: 'Fix bug',
+        day: 'Feb 5th at 2.30 pm',
+        reminder: true
+      },
+      {
+        id: 3,
+        text: 'Fix bug',
+        day: 'Feb 5th at 2.30 pm',
+        reminder: true
+      },
+    ]
+  );
   return (
     <div className="container">
       <Header/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
